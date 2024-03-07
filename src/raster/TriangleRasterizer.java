cc591cab909c4f7772167d85f8f160973062e215
Lukas Matejovsky <lukyn.ma@gmail.com>
2024-03-07 21:04:23 +0100

    Doplnění druhé části rasterizace

--- src/raster/TriangleRasterizer.java
+++ src/raster/TriangleRasterizer.java
@@ -48,9 +48,9 @@ public class TriangleRasterizer {
             //double z2 = (1 - t2) * zA + t2 * zC;
             Col col2 = a.getColor().mul(1 - t2).add(c.getColor().mul(t2));
 
-            // TODO: kontrola, jestli x1 < x2
+            // Kontrola, jestli x1 < x2
             for (int x = x1; x <= x2; x++) {
-                double t3 = (x - x1) / (double)(x2 - x1);
+                double t3 = (x - x1) / (double) (x2 - x1);
                 //double z = (1 - t3) * z1 + t3 * z2;
                 Col col = col1.mul(1 - t3).add(col2.mul(t3));
 
@@ -58,7 +58,29 @@ public class TriangleRasterizer {
             }
         }
 
-        // TODO: Cyklus od B do C (druhá část)
+        //Cyklus od B do C (druhá část)
+        for (int y = yB; y <= (int) c.getPosition().getY(); y++) {
+            // V1
+            double t1 = (y - yB) / (double) (yC - yB);
+            int x1 = (int) Math.round((1 - t1) * xB + t1 * xC);
+            //double z1 = (1 - t1) * zA + t1 * zB;
+            Col col1 = b.getColor().mul(1 - t1).add(c.getColor().mul(t1));
+
+            // V2
+            double t2 = (y - yA) / (double) (yC - yA);
+            int x2 = (int) Math.round((1 - t2) * xA + t2 * xC);
+            //double z2 = (1 - t2) * zA + t2 * zC;
+            Col col2 = a.getColor().mul(1 - t2).add(c.getColor().mul(t2));
+
+            //Kontrola, jestli x1 < x2
+            for (int x = x1; x <= x2; x++) {
+                double t3 = (x - x1) / (double) (x2 - x1);
+                //double z = (1 - t3) * z1 + t3 * z2;
+                Col col = col1.mul(1 - t3).add(col2.mul(t3));
+
+                zBuffer.setPixelWithZTest(x, y, 0.5, col);
+            }
+        }
 
     }
 }
