1c986e05ce09d5d2d6ba1b02e70f2b0363543027
Lukas Matejovsky <lukyn.ma@gmail.com>
2024-04-25 20:05:48 +0200

    Rozšíření rendereru o dva další rasterizery a rozšířil jsem o linie

--- src/control/Controller3D.java
+++ src/control/Controller3D.java
@@ -1,27 +1,46 @@
 package control;
 
-import raster.ImageBuffer;
-import raster.Raster;
-import raster.TriangleRasterizer;
-import raster.ZBuffer;
-import solid.Vertex;
+import raster.*;
+import render.Renderer3D;
+import solid.Minecraft;
+import solid.Pyramid;
+import solid.Solid;
 import transforms.*;
 import view.Panel;
 
-import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Controller3D implements Controller {
     private final Panel panel;
+
+    private Renderer3D renderer3D;
     private ZBuffer zBuffer;
     private TriangleRasterizer triangleRasterizer;
+    private LineRasterizerTrivial lineRasterizerTrivial;
     private BufferedImage texture;
 
     private Mat4 model, projection;
     private Camera camera;
+
+    private Solid pyramid;
+    private Solid minecraft;
+
+    private int firstX;
+    private int firstY;
+
+    private int secondX;
+    private int secondY;
+    double azimut = 90;
+    double zenit = 0;
+
+    int pX = 0;
+    int pY = -2;
+    double pZ = 0.3;
+    final double step = 0.25;
+
+    private String projType = "persp";
 
     public Controller3D(Panel panel) {
         this.panel = panel;
@@ -35,20 +54,37 @@ public class Controller3D implements Controller {
 
         zBuffer = new ZBuffer((ImageBuffer) raster);
         triangleRasterizer = new TriangleRasterizer(zBuffer);
+        lineRasterizerTrivial = new LineRasterizerTrivial(zBuffer);
+        renderer3D = new Renderer3D(triangleRasterizer, lineRasterizerTrivial);
 
-        model = new Mat4Identity();
+//        model = new Mat4Identity();
+//
+//        Vec3D e = new Vec3D(1, -5, -1);
+//        camera = new Camera()
+//                .withPosition(e)
+//                .withAzimuth(Math.toRadians(90))
+//                .withZenith(Math.toRadians(0));
+//
+//        projection = new Mat4PerspRH(
+//            Math.PI / 3,
+//            raster.getHeight() / (float) raster.getWidth(),
+//            0.5,
+//            50
+//        );
 
-        Vec3D e = new Vec3D(1, -5, 2);
-        camera = new Camera()
-                .withPosition(e)
-                .withAzimuth(Math.toRadians(90))
-                .withZenith(Math.toRadians(-15));
+        camera = new Camera(
+                new Vec3D(pX, pY, pZ),
+                Math.toRadians(azimut),
+                Math.toRadians(zenit),
+                1,
+                true
+        );
 
         projection = new Mat4PerspRH(
-            Math.PI / 3,
-            raster.getHeight() / (float) raster.getWidth(),
-            0.5,
-            50
+                Math.PI / 4,
+                raster.getHeight() / (double) raster.getWidth(),
+                0.1,
+                20
         );
     }
 
@@ -61,16 +97,144 @@ public class Controller3D implements Controller {
                 initObjects(panel.getRaster());
             }
         });
+
+        panel.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                if (e.isControlDown()) return;
+
+                if (e.isShiftDown()) {
+                    //TODO
+                } else if (SwingUtilities.isLeftMouseButton(e)) {
+                    firstX = e.getX();
+                    firstY = e.getY();
+                    // rasterizer.rasterize(x, y, e.getX(),e.getY(), Color.RED);
+                } else if (SwingUtilities.isMiddleMouseButton(e)) {
+                    //TODO
+                } else if (SwingUtilities.isRightMouseButton(e)) {
+                    //TODO
+                }
+
+                redraw();
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.isControlDown()) {
+                    if (SwingUtilities.isLeftMouseButton(e)) {
+                        //TODO
+                    } else if (SwingUtilities.isRightMouseButton(e)) {
+                        //TODO
+                    }
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+        });
+
+        panel.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (e.isControlDown()) return;
+
+                if (e.isShiftDown()) {
+                    //TODO
+                } else if (SwingUtilities.isLeftMouseButton(e)) {
+                    secondX = e.getX();
+                    secondY = e.getY();
+
+                    int dx = secondX - firstX;
+                    int dy = secondY - firstY;
+
+                    zenit -= (double) 180 * dy / panel.getHeight();
+                    if (zenit > 90) zenit = 90;
+                    if (zenit < -90) zenit = -90;
+
+
+                    azimut -= (double) 180 * dx / panel.getWidth();
+                    azimut = azimut % 360;
+
+                    firstX = secondX;
+                    firstY = secondY;
+                } else if (SwingUtilities.isRightMouseButton(e)) {
+                    //TODO
+                } else if (SwingUtilities.isMiddleMouseButton(e)) {
+                    //TODO
+                }
+                redraw();
+            }
+        });
+
+        panel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // na klávesu C vymazat plátno
+                if (e.getKeyCode() == KeyEvent.VK_C) {
+                    //TODO
+                } else if (e.getKeyCode() == KeyEvent.VK_O) {
+                    projection = new Mat4OrthoRH(
+                            3,
+                            3,
+                            0.1,
+                            20
+                    );
+                    projType = "ort";
+                } else if (e.getKeyCode() == KeyEvent.VK_P) {
+                    projection = new Mat4PerspRH(
+                            Math.PI / 4,
+                            panel.getRaster().getHeight() / (double) panel.getRaster().getWidth(),
+                            0.1,
+                            20
+                    );
+                    projType = "persp";
+                } else if (e.getKeyCode() == KeyEvent.VK_W) {
+                    camera = camera.forward(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_A) {
+                    camera = camera.left(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                    camera = camera.right(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                    camera = camera.backward(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    camera = camera.up(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+                    camera = camera.down(step);
+                } else if (e.getKeyCode() == KeyEvent.VK_M) {
+                    renderer3D.changeRasterizer();
+                }
+                redraw();
+            }
+        });
     }
 
     private void redraw() {
         panel.clear();
+        zBuffer.setDefault();
 
-        triangleRasterizer.rasterize(
-                new Vertex(new Point3D(0, 300, 0.5), new Col(0xff0000), new Vec2D(0.5, 1.)),
-                new Vertex(new Point3D(400, 0, 0.5), new Col(0x00ff00), new Vec2D(0, .5)),
-                new Vertex(new Point3D(799, 599, 0.5), new Col(0x0000ff), new Vec2D(1., 0.))
-        );
+        renderer3D.setProjection(projection);
+
+        camera = camera.withAzimuth(Math.toRadians(azimut)).withZenith(Math.toRadians(zenit));
+        renderer3D.setView(camera.getViewMatrix());
+
+        Mat4 matTrans = new Mat4Identity();
+        matTrans = matTrans.mul(new Mat4RotZ(45));
+
+        Mat4 matTrans2 = new Mat4Identity();
+        matTrans2 = matTrans.mul(new Mat4Transl(1, 0, 0));
+
+        pyramid = new Pyramid();
+        pyramid.setModel(matTrans);
+
+        minecraft = new Minecraft();
+        minecraft.setModel(matTrans2);
+
+        renderer3D.render(pyramid);
+        renderer3D.render(minecraft);
 
         panel.repaint();
     }
--- src/render/Renderer3D.java
+++ src/render/Renderer3D.java
@@ -1,16 +1,41 @@
 package render;
 
+import raster.LineRasterizerTrivial;
+import raster.Rasterizer;
 import raster.TriangleRasterizer;
 import solid.Part;
 import solid.Solid;
 import solid.Vertex;
 import transforms.Mat4;
 
+import java.util.List;
+
 public class Renderer3D {
     private Mat4 model;
 
-    public void setModel(Mat4 model) {
-        this.model = model;
+    private Mat4 view;
+    private Mat4 projection;
+
+    private Mat4 matTransformation;
+    private TriangleRasterizer triangleRasterizer;
+    private LineRasterizerTrivial lineRasterizer;
+
+    private Rasterizer rasterizer;
+
+    private boolean isWired = false;
+
+    public Renderer3D(TriangleRasterizer triangleRasterizer, LineRasterizerTrivial lineRasterizer) {
+        this.rasterizer = triangleRasterizer;
+        this.triangleRasterizer = triangleRasterizer;
+        this.lineRasterizer = lineRasterizer;
+    }
+    public void changeRasterizer() {
+        if (isWired) {
+            this.rasterizer = lineRasterizer;
+        } else {
+            this.rasterizer = triangleRasterizer;
+        }
+        isWired = !isWired;
     }
 
     public void setView(Mat4 view) {
@@ -21,19 +46,6 @@ public class Renderer3D {
         this.projection = projection;
     }
 
-    private Mat4 view;
-    private Mat4 projection;
-
-    private Mat4 matTransformation;
-
-    private TriangleRasterizer triangleRasterizer;
-    // TODO: proj, view
-
-
-    public Renderer3D(TriangleRasterizer triangleRasterizer) {
-        this.triangleRasterizer = triangleRasterizer;
-    }
-
     public void render(Solid solid) {
         // TODO: transformace
         matTransformation = solid.getModel().mul(view).mul(projection);
@@ -41,7 +53,16 @@ public class Renderer3D {
         for (Part part : solid.getPartBuffer()) {
             switch (part.getType()) {
                 case LINES:
-                    // TODO: implementovat
+                    int startLine = part.getStart();
+                    for (int i = 0; i < part.getCount(); i++) {
+                        int indexA = startLine;
+                        int indexB = startLine + 1;
+                        startLine += 2;
+
+                        Vertex a = solid.getVertexBuffer().get(solid.getIndexBuffer().get(indexA));
+                        Vertex b = solid.getVertexBuffer().get(solid.getIndexBuffer().get(indexB));
+                        clipLine(a, b);
+                    }
                     break;
                 case TRIANGLES:
                     int start = part.getStart();
@@ -51,9 +72,9 @@ public class Renderer3D {
                         int indexC = start + 2;
                         start += 3;
 
-                        Vertex a = solid.getVertexBuffer().get(indexA);
-                        Vertex b = solid.getVertexBuffer().get(indexB);
-                        Vertex c = solid.getVertexBuffer().get(indexC);
+                        Vertex a = solid.getVertexBuffer().get(solid.getIndexBuffer().get(indexA));
+                        Vertex b = solid.getVertexBuffer().get(solid.getIndexBuffer().get(indexB));
+                        Vertex c = solid.getVertexBuffer().get(solid.getIndexBuffer().get(indexC));
                         clipTriangle(a, b, c);
                     }
                     break;
@@ -67,7 +88,7 @@ public class Renderer3D {
         Vertex nB = new Vertex(b.getPosition().mul(matTransformation), b.getColor(), b.getUv());
         Vertex nC = new Vertex(c.getPosition().mul(matTransformation), c.getColor(), c.getUv());
 
-        // TODO: fast clip
+        //fast clip
         if (nA.getPosition().getX() > nA.getPosition().getW() && nB.getPosition().getX() > nB.getPosition().getW() && nC.getPosition().getX() > c.getPosition().getW()) return;
         if (nA.getPosition().getX() < -nA.getPosition().getW() && nB.getPosition().getX() < -nB.getPosition().getW() && nC.getPosition().getX() < -c.getPosition().getW()) return;
         if (nA.getPosition().getY() > nA.getPosition().getW() && nB.getPosition().getY() > nB.getPosition().getW() && nC.getPosition().getY() > c.getPosition().getW()) return;
@@ -77,7 +98,7 @@ public class Renderer3D {
         if (nA.getPosition().getZ() < 0 && nB.getPosition().getZ() < 0 && nC.getPosition().getZ() < 0) return;
 
 
-        // TODO: sežadit vrcholy podle z, aby aZ = max
+        //seřazení vrcholy podle z, aby aZ = max
         if (nA.getPosition().getZ() < nB.getPosition().getZ()) {
             Vertex tmp = nA;
             nA = nB;
@@ -108,7 +129,8 @@ public class Renderer3D {
             double tv2 = (zMin - nA.getPosition().getZ()) / (nC.getPosition().getZ() - nA.getPosition().getZ());
             Vertex v2 = nA.mul(1 - tv2).add(nC.mul(tv2));
 
-            triangleRasterizer.rasterize(nA, v1, v2);
+            //triangleRasterizer.rasterize(nA, v1, v2);
+            rasterizer.rasterize(nA, v1, v2);
             return;
         }
 
@@ -119,13 +141,60 @@ public class Renderer3D {
             double tv2 = -nB.getPosition().getZ() / (nC.getPosition().getZ() - nB.getPosition().getZ());
             Vertex v2 = nB.mul(1 - tv2).add(nC.mul(tv1));
 
-            triangleRasterizer.rasterize(nA, nB, v2);
-            triangleRasterizer.rasterize(nA, v1, v2);
+//            triangleRasterizer.rasterize(nA, nB, v2);
+//            triangleRasterizer.rasterize(nA, v1, v2);
+            rasterizer.rasterize(nA, nB, v2);
+            rasterizer.rasterize(nA, v1, v2);
             return;
         }
 
-        triangleRasterizer.rasterize(nA, nB, nC);
+//        triangleRasterizer.rasterize(nA, nB, nC);
+        rasterizer.rasterize(nA, nB, nC);
     }
 
-    // TODO: metoda render pro seznam solidů
+    private void clipLine(Vertex a, Vertex b) {
+        //Trasformace až do projekce
+        Vertex nA = new Vertex(a.getPosition().mul(matTransformation), a.getColor(), a.getUv());
+        Vertex nB = new Vertex(b.getPosition().mul(matTransformation), b.getColor(), b.getUv());
+
+        //fast clip
+        if (nA.getPosition().getX() > nA.getPosition().getW() && nB.getPosition().getX() > nB.getPosition().getW()) return;
+        if (nA.getPosition().getX() < -nA.getPosition().getW() && nB.getPosition().getX() < -nB.getPosition().getW()) return;
+        if (nA.getPosition().getY() > nA.getPosition().getW() && nB.getPosition().getY() > nB.getPosition().getW()) return;
+        if (nA.getPosition().getY() < -nA.getPosition().getW() && nB.getPosition().getY() < -nB.getPosition().getW()) return;
+
+        if (nA.getPosition().getZ() > nA.getPosition().getW() && nB.getPosition().getZ() > nB.getPosition().getW()) return;
+        if (nA.getPosition().getZ() < 0 && nB.getPosition().getZ() < 0) return;
+
+
+        //seřazení vrcholy podle z, aby aZ = max
+        if (nA.getPosition().getZ() < nB.getPosition().getZ()) {
+            Vertex tmp = nA;
+            nA = nB;
+            nB = tmp;
+        }
+
+        double zMin = 0;
+
+        if(nA.getPosition().getZ() < zMin)
+            return;
+
+        if(nB.getPosition().getZ() < zMin) {
+            double tv1 = (zMin - nB.getPosition().getZ()) / (nB.getPosition().getZ() - nA.getPosition().getZ());
+            Vertex v1 = nA.mul(1 - tv1).add(nB.mul(tv1));
+
+            //triangleRasterizer.rasterize(nA, v1, v2);
+            lineRasterizer.rasterize(nA, v1);
+            return;
+        }
+//        triangleRasterizer.rasterize(nA, nB, nC);
+        lineRasterizer.rasterize(nA, nB);
+    }
+
+    //metoda render pro seznam solidů
+    public void render(List<Solid> scene) {
+        for (Solid s : scene) {
+            render(s);
+        }
+    }
 }
